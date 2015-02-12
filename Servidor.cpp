bfde35606d776de20f8d64da09f9c85a17220750
Christian Camilo Restrepo <ckrestrepo@gmail.com>
2015-02-12 11:26:55 -0500

    Update Servidor.cpp

--- Servidor.cpp
+++ Servidor.cpp
@@ -1,5 +1,5 @@
 /* ..... SERVIDOR ....*/
-
+#include "unistd.h"
 #include "iostream"
 #include "stdlib.h"
 #include "string.h"
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-#define MIPUERTO 9999   //pto de conexion para usuarios
+#define MIPUERTO 9090   //pto de conexion para usuarios
 #define MIIP "127.0.0.1"
 #define ENCOLA 10       //numero conexiones permitidas
 #define MAXLONGITUD 128
@@ -21,11 +21,11 @@ int main ()
     int nosocket, nuevo_socket;
     struct sockaddr_in dirservidor;     // info sobre mi direccion   
     struct sockaddr_in cliente;         // info sobre conexiones entrantes
-    socklen_t señal_tam;                // tamaño de  la señal
+    socklen_t senal_tam;                // tamaño de  la señal
     
     // llamada al socket
     nosocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (noscoket == -1)
+    if (nosocket == -1)
     {
         cout<<"Error de socket"<<endl;
         exit(-1);
@@ -39,7 +39,7 @@ int main ()
     //inet_aton("localhost", &(dirservidor.sin_addr));
     //dirservidor.sin_addr.s_addr = inet_addr(MIIP);
     dirservidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-    bzero(&dirservidor.sin_zero),8);
+    bzero(&(dirservidor.sin_zero),8);
     //memset(&dirservidor.sin_zero), 0, 8);
     
     if (bind(nosocket, (struct sockaddr*)&dirservidor, sizeof(struct sockaddr))==-1)
@@ -55,8 +55,8 @@ int main ()
     cout <<"nosotros: " <<nosocket <<endl;
     while(1)
     {
-        señal_tam = sizeof(struct sockaddr_in);
-        nuevo_socket = accept(nosocket, (struct sockaddr*)&cliente, &señal_tam);
+        senal_tam = sizeof(struct sockaddr_in);
+        nuevo_socket = accept(nosocket, (struct sockaddr*)&cliente, &senal_tam);
         if (nuevo_socket == -1)
         {
             cout <<"error en accept()" << endl;
