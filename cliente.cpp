2575e435c96c6332b4f5b49ca7beb2a8218bded9
ckrestrepo <ckrestrepo@gmail.com>
2015-02-19 23:15:44 -0500

    Primera version

---
+++ cliente.cpp
@@ -0,0 +1,151 @@
+/* ..... CLIENTE ....*/
+
+#include "stdio.h"
+#include "iostream"
+#include "stdlib.h"
+#include "string.h"
+#include "malloc.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "netdb.h"
+#include "unistd.h"
+#include "sstream"
+
+using namespace std;
+
+#define MIPUERTO 9091
+#define MAXLONGITUD 128
+
+int nosocket, numbytes;
+char msj[MAXLONGITUD];
+char const host_servidor[]="127.0.0.1";
+struct hostent *host_entrante;
+struct sockaddr_in servidor;
+
+// Funciones declaradas
+int iniciar();
+void conectar();
+string recibir(int);
+void enviar(int, string);
+
+
+//Metodos
+int iniciar()
+{
+	host_entrante = gethostbyname(host_servidor);
+	if (host_entrante == NULL)
+	{
+		// llamada a gethostbyname()
+		cout << "Error al recibir host" <<endl;
+		exit(-1);
+	}
+	
+	nosocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (nosocket == -1)
+	{
+		cout <<"Error en conexion 1" <<endl;
+		exit(-1);
+	}
+	servidor.sin_family = AF_INET;
+	servidor.sin_port = htons(MIPUERTO);
+	servidor.sin_addr = *((struct in_addr *)host_entrante ->h_addr);
+	//he pasa la informacion de *he hacia h_addr
+	bzero(&(servidor.sin_zero), 8);
+}
+
+void conectar()
+{
+	if (connect(nosocket, (struct sockaddr *)&servidor, sizeof(struct sockaddr)) == -1)
+	{
+		cout <<"Error en conexion 2" <<endl;
+		exit(-1);
+	}
+}
+
+void enviar (int sock, string cadena)
+{
+    char mensaje[MAXLONGITUD];
+    strcpy(mensaje, cadena.c_str());
+    send(sock, mensaje, cadena.size(), 0);
+}
+
+string recibir (int sock)
+{
+    char msj[MAXLONGITUD];
+    string mensaje = ""; //toca inicializarlo en vacio
+    numbytes = recv (sock, msj, MAXLONGITUD, 0);
+    if (numbytes == -1)
+    {
+        cout <<"Error al recibir. " <<endl;
+    }
+    if (numbytes == 0)
+    {
+        cout <<"Conexion finalizada" <<endl;
+    }
+    if (numbytes > 0)
+    {
+        msj[numbytes] = '\0'; //con el backslash cero indica que hasta ahi llega el mensaje
+        mensaje = (string)msj;
+    }
+    return mensaje;
+}
+
+// Convierte de cadena a entero
+int stringtoint(string x)
+{
+	int valor = atoi(x.c_str());
+	return valor;
+}
+
+//Convierte de entero a cadena
+string inttostring(int x)
+{
+	stringstream ss;
+	ss<<x;
+	string cadena = ss.str();
+	return cadena;
+}
+
+int main()
+{	
+	iniciar();
+	conectar();
+	cout<<"Mensaje: " <<recibir(nosocket) <<endl;
+	//Ciclo del servicio
+	int opcion, a, b;
+	do
+	{
+		do
+		{
+			cout <<"\n...Menu de Opciones...\n";
+			cout <<"1. Sumar\n";
+			cout <<"2. Contar caracteres\n";
+			cout <<"3. Salir...\n";
+			cout <<"Digite una opcion: ";
+			cin >> opcion;
+			/*
+			if (opcion == 1)
+			{
+				cout <<"Digite el primer numero: ";
+				cin >> b;
+				enviar(nosocket, inttostring(b));
+				cout <<recibir(nosocket);
+				cout <<"Digite el segundo numero: ";
+			}*/
+		}while (opcion < 0 || opcion > 3);
+		enviar(nosocket, inttostring(opcion));
+		cout <<recibir(nosocket)<<endl;
+	}while(opcion != 3);
+
+	/*
+	string msjenv;
+	msj[0] ='\0';
+	cout <<"Digite cualquier cosa: ";
+	cin >> msjenv;
+	strcpy(msj, msjenv.c_str()); // c_str() convierte a char
+	send(nosocket, msj, msjenv.size(), 0);
+	*/
+	close(nosocket);
+}
---
+++ servidor.cpp
@@ -0,0 +1,169 @@
+/* ..... SERVIDOR ....*/
+#include "unistd.h"
+#include "iostream"
+#include "stdlib.h"
+#include "string.h"
+#include "malloc.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "sstream"
+
+using namespace std;
+
+#define MIPUERTO 9091   //pto de conexion para usuarios
+#define MIIP "127.0.0.1"
+#define ENCOLA 10       //numero conexiones permitidas
+#define MAXLONGITUD 128
+
+int inicializar();
+void verIP(struct sockaddr_in, int);
+int nueva_cx(int);
+string recibir(int);
+void enviar(int);
+void enviar(int, string);
+int stringtoint(string);
+string inttostring(int);
+
+
+int nosocket, nuevo_socket;
+struct sockaddr_in dirservidor;
+struct sockaddr_in cliente;
+socklen_t senal_tam;
+
+
+int main ()
+{
+    inicializar();
+    cout <<"\n...Bienvenido al servidor...\n";
+    while(1)
+    {
+        nuevo_socket = nueva_cx(nosocket);
+        verIP(cliente, nuevo_socket);
+        int opcion;
+        do
+        {
+            string valoropc = recibir(nuevo_socket);
+            cout <<"Cliente dice: " << valoropc <<endl;
+            //Cambiamos la cadena a entero
+            opcion = stringtoint(valoropc);
+            enviar(nuevo_socket, "Acciona realizada");
+            /*
+            if (opcion == 1)
+            {
+                enviar(nuevo_socket, "numero 1 recibido\n");
+            }*/
+        }while(opcion != 3);
+        close(nuevo_socket);
+    } // fin del while (1)
+    return 0;
+}
+
+// Funcion que suma dos numeros
+int suma (int a, int b)
+{
+    int res = a + b;
+    return res;
+}
+
+// Funcion que inicializa el servidor
+int inicializar ()
+{
+    nosocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (nosocket == -1)
+    {
+        cout <<"Error de Socket..."<<endl;
+        exit(-1);
+    }
+    dirservidor.sin_family = AF_INET;
+    dirservidor.sin_port = htons(MIPUERTO);
+    dirservidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bzero(&(dirservidor.sin_zero),8);
+    if (bind(nosocket, (struct sockaddr*)&dirservidor, sizeof(struct sockaddr))==-1)
+    {
+        cout <<"Error en bind()" <<endl;
+        exit(-1);
+    }
+    if (listen(nosocket,ENCOLA) == -1)
+    {
+        cout <<"Error en listen()" <<endl;
+        exit(-1);
+    }
+}
+
+// Funcion que hace una nueva conexion
+int nueva_cx (int sock)
+{
+    senal_tam = sizeof(struct sockaddr_in);
+    int ns = accept(sock, (struct sockaddr*)&cliente, &senal_tam);
+    if (nuevo_socket == -1)
+    {
+        cout <<"error en accept()" << endl;
+        exit(-1);
+    }
+    return ns;
+}
+
+// Funcion que permite ver la IP del cliente
+void verIP (struct sockaddr_in c, int sock)
+{
+    char*  dire;
+    dire = inet_ntoa(c.sin_addr);
+    cout <<"Direccion del cliente: " <<dire <<endl;
+    enviar(sock, "\n...Bienvenido al servidor...\n");
+}
+
+// Funcion que envia mensajes al cliente (tiene sobrecarga)
+// digamos dos constructores
+
+void enviar (int sock)
+{
+    send(sock, "Breve la vuelta...\n", 19, 0);
+}
+
+void enviar (int sock, string cadena)
+{
+    char mensaje[MAXLONGITUD];
+    strcpy(mensaje, cadena.c_str());
+    send(sock, mensaje, cadena.size(), 0);
+}
+
+// Funcion que recive los mensajes
+string recibir (int sock)
+{
+    char msj[MAXLONGITUD];
+    string mensaje = ""; //toca inicializarlo en vacio
+    int numbytes = recv (sock, msj, MAXLONGITUD, 0);
+    if (numbytes == -1)
+    {
+        cout <<"Error al recibir. " <<endl;
+    }
+    if (numbytes == 0)
+    {
+        cout <<"Conexion finalizada" <<endl;
+    }
+    if (numbytes > 0)
+    {
+        msj[numbytes] = '\0'; //con el backslash cero indica que hasta ahi llega el mensaje
+        //cout <<"Numero: " <<msj <<endl;
+        mensaje = (string)msj;
+    }
+    return mensaje;
+}
+
+int stringtoint(string x)
+{
+    int valor = atoi(x.c_str());
+    return valor;
+}
+
+//Convierte de entero a cadena
+string inttostring(int x)
+{
+    stringstream ss;
+    ss<<x;
+    string cadena = ss.str();
+    return cadena;
+}
+
