87c806cb4fc52cb8a3325d4eb143fd528dd7547a
ckrestrepo <ckrestrepo@gmail.com>
2015-04-08 06:52:17 -0500

    Comunicacion de hijos con el padre, etc..

---
+++ comunicacionExterna.cpp
@@ -0,0 +1,13 @@
+/* ..... SERVIDOR CON PROCESOS ....*/
+
+#include "stdio.h"
+#include "unistd.h"
+#include "iostream"
+#include "stdlib.h"
+#include "string.h"
+#include "malloc.h"
+#include "sys/types.h"
+#include "wait.h"
+#include "sstream"
+
+using namespace std;
---
+++ pro2server.cpp
@@ -0,0 +1,197 @@
+/* ..... SERVIDOR CON PROCESOS ....*/
+
+#include "stdio.h"
+#include "unistd.h"
+#include "iostream"
+#include "stdlib.h"
+#include "string.h"
+#include "malloc.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "sstream"
+#include "wait.h"
+#include "list"
+
+using namespace std;
+
+#define MIPUERTO 9090   //pto de conexion para usuarios
+#define MIIP "192.168.8.79"
+#define ENCOLA 10       //numero conexiones permitidas
+#define MAXLONG 256
+
+//Estructuras
+struct usuarios
+{
+	string user;
+	string ip;
+	int idint;
+};
+
+list<usuarios> users;
+usuarios us;
+
+//Definicion de Funciones
+int inicializar();
+int Recibir (int);
+void Responder (int);
+void Enviar (int, struct sockaddr_in);
+int nueva_cx (int);
+void mostrar(list<usuarios>);
+
+
+//Variables "globales"
+int nosocket, nuevo;
+struct sockaddr_in dirservidor;
+struct sockaddr_in cliente;
+socklen_t lon;
+
+int main()
+{
+	inicializar();
+    cout <<"\n...Servidor Iniciado...\n";
+	int fin = 1;
+	do
+	{
+		nuevo = nueva_cx(nosocket);
+		pid_t idProceso;
+		int estadoHijo;
+		idProceso = fork();
+		if(idProceso == -1)
+		{
+			perror ("No se puede crear el proceso\n");
+			exit(-1);
+		}
+		//Hijo
+		if (idProceso == 0)
+		{
+			int estado = 0;
+			//Ciclo de servicio del Hijo
+			do
+			{
+				estado = Recibir(nuevo);
+				if(estado == 1)
+				{
+					Responder(nuevo);
+				}
+				if(estado == 2)
+				{
+					//Enviar la lista de usuarios
+					Responder(nuevo);
+					//Enviar(info);
+				}
+			}while ((estado != 33) && (estado != -1));
+			exit (33);
+		}
+		//Padre
+		if(idProceso > 0)
+		{
+			Enviar(nuevo, cliente);
+			us.user = "vacio";
+			char *dire;
+			dire = inet_ntoa(cliente.sin_addr);
+			us.ip = (string)dire;
+			us.idint = nuevo;
+			users.push_back(us);
+			mostrar(users);
+		}
+	}while (fin == 1);
+	return 0;
+}
+
+
+void mostrar(list<usuarios> usr)
+{
+	int registro = 1;
+	cout <<"Cantidad de Usuarios: " <<usr.size() <<endl;
+	list <usuarios>:: iterator i = usr.begin();
+	while (i!= usr.end())
+	{
+		//usuarios us este es el global
+		us =*i;
+		cout <<"\nRegistro: " << registro <<endl;;
+		cout <<"Usuario: "<< us.user<< endl;
+		cout <<"ID interno: "<< us.idint <<endl;
+		cout <<"Direccion IP: "<< us.ip <<endl;
+		i++;
+		registro++;
+	}	
+}
+
+int inicializar ()
+{
+    nosocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (nosocket == -1)
+    {
+        cout <<"Error de Socket..."<<endl;
+        exit(-1);
+    }
+    dirservidor.sin_family = AF_INET;
+    dirservidor.sin_port = htons(MIPUERTO);
+    dirservidor.sin_addr.s_addr = inet_addr("192.168.8.79");
+    bzero(&(dirservidor.sin_zero),8);
+    if (bind(nosocket, (struct sockaddr*)&dirservidor, sizeof(struct sockaddr))==-1)
+    {
+        cout <<"Error en bind()" <<endl;
+        exit(-1);
+    }
+    if (listen(nosocket,ENCOLA) == -1)
+    {
+        cout <<"Error en listen()" <<endl;
+        exit(-1);
+    }
+}
+
+// Funcion que hace una nueva conexion
+int nueva_cx (int sock)
+{
+    lon = sizeof(struct sockaddr_in);
+    int ns = accept(sock, (struct sockaddr*)&cliente, &lon);
+    if (nuevo == -1)
+    {
+        cout <<"error en accept()" << endl;
+        exit(-1);
+    }
+    return ns;
+}
+
+int Recibir (int ns)
+{
+	int nb, salida;
+	char msj[MAXLONG];
+	nb = recv(ns, msj, MAXLONG, 0);
+	if(nb == -1)
+	{
+		cout <<"Error al recibir.\n";
+		salida = -1;
+	}
+	if (nb == 0)
+	{
+		cout <<"Conexion finalizada\n";
+		close(ns);
+		salida = 33;
+	}
+	if (nb > 0)
+	{
+		msj[nb] = '\0';
+		cout <<"Mensaje del cliente [" <<ns <<"]: " <<msj <<endl;
+		msj[0] = '\0';
+		salida = 1;
+	}
+	return salida;
+}
+
+void Responder (int ns)
+{
+    send(ns, "Breve la vuelta...\n", 19, 0);
+}
+
+void Enviar (int ns, struct sockaddr_in cl)
+{
+	char *dire;
+	dire = inet_ntoa(cl.sin_addr);
+	send(ns, "\n....Bienvenido al Servidor ...\n", 34, 0);
+	cout <<"Direccion entrante: "<<dire <<endl;
+}
+
